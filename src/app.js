d897c69cd8ad70c46264b6a15bd726e0588a609f
Carlosygaby <191144185+Carlosygaby@users.noreply.github.com>
2025-03-03 20:16:07 +0100

    corrigiendo error en linea 30

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ window.onload = function() {
 
   function generateExcuse() {
     function getRandomNumber(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
+      return Math.floor(Math.random() * (max - min +1) + min);
     }
 
     function getRandom(anyArray) {
